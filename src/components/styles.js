20ba6347eaac26e789e74d79467620efcd80f870
Patrick Nepp <ptnepp@gmail.com>
2023-03-27 20:54:56 -0500

    sidebar

--- src/components/styles.js
+++ src/components/styles.js
@@ -3,11 +3,12 @@ import { makeStyles } from '@mui/styles';
 export default makeStyles(() => ({
   root: {
     display: 'flex',
-    height: '100px',
+    height: '100%',
   },
   content: {
     flexGrow: 1,
     padding: '2em',
+    width: '100%',
   },
   toolbar: {
     height: '70px',
