49163d622835e148c51e46179fff175cdbff8dc1
Patrick Nepp <ptnepp@gmail.com>
2023-03-27 21:52:33 -0500

    redux set up

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
-  console.log('Movies');
+  const { data } = useGetMoviesQuery();
+
+  console.log(data);
+
   return (
-    <div>Movies</div>
+    <div>
+      <h1>Movies</h1>
+    </div>
   );
 };
 
