28419583ac312a71c2b86624da1a6bc16adf0b36
Patrick Nepp <ptnepp@gmail.com>
2023-04-14 19:01:57 -0500

    featured movie card

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,12 +4,13 @@ import { Grid } from '@mui/material';
 import useStyles from './styles';
 import Movie from '../Movie/Movie';
 
-const MovieList = ({ movies, numOfMovies }) => {
+const MovieList = ({ movies, numOfMovies, excludeFirst }) => {
   const classes = useStyles();
+  const startFrom = excludeFirst ? 1 : 0;
 
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.results.slice(0, numOfMovies).map((movie, index) => (
+      {movies.results.slice(startFrom, numOfMovies).map((movie, index) => (
         <Movie key={index} movie={movie} index={index} />
       ))}
     </Grid>
